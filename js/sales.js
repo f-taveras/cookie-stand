7e71136d583963f2607a7ef7bd363514299c9667
Felix A. Taveras <felixataveras1@gmail.com>
2023-11-02 11:26:28 -0400

    saving

--- js/sales.js
+++ js/sales.js
@@ -1,15 +1,17 @@
-// Function to generate a random number between min and max (inclusive)
+// Function to generate a random number between min and max 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Constructor function to create store info
-function StoreInfo(name, minCustomers, maxCustomers, avgCookie) {
+function StoreInfo(name, minCustomers, maxCustomers, avgCookie,) {
   this.name = name;
   this.minCustomersPerHour = minCustomers;
   this.maxCustomersPerHour = maxCustomers;
   this.averageCookies = avgCookie;
   this.render();
+  this.totalPerHour = []
+  this.totalPerDay = 0
 }
 
 StoreInfo.prototype.render = function() {
@@ -27,11 +29,12 @@ StoreInfo.prototype.render = function() {
     let cell = document.createElement("td");
     cell.textContent = salesData[i];
     storeRow.appendChild(cell);
+    
   }
 }
 
+let hours = ["6am", "7am", "8am","9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm"];
 StoreInfo.prototype.estimate = function() {
-  let hours = ["6am", "7am", "8am","9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm"];
   let estimatedSales = [];
 
   for (let i = 0; i < hours.length; i++) {
@@ -41,6 +44,9 @@ StoreInfo.prototype.estimate = function() {
   }
 
   return estimatedSales;
+  
+
+
 }
 
 let seattle = new StoreInfo("Seattle", 23, 65, 6.3);
@@ -48,8 +54,6 @@ let tokyo = new StoreInfo("Tokyo", 3, 24, 1.2);
 let dubai = new StoreInfo("Dubai", 11, 24, 3.7);
 let paris = new StoreInfo("Paris", 20, 38, 2.3);
 let lima = new StoreInfo("Lima", 2, 16, 4.6);
+// let totalSales = new StoreInfo("Horly Totals for All Locatoins",)
 
 
-// let salesTotals = totals(){
-
-// }
